f0b22f2f9db42504936b49873b5d04c4dc787048
anuragchaudharyusar <141890245+anuragchaudharyusar@users.noreply.github.com>
2025-07-05 13:54:01 +0000

    day 5 commit (still worikng on postman)

---
+++ backend/controllers/authController.js
@@ -0,0 +1,59 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+// Create JWT
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: '7d' }
+  );
+};
+
+// @route   POST /api/register
+exports.registerUser = async (req, res) => {
+  try {
+    const { username, email, password, role } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ msg: 'User already exists' });
+
+    const user = await User.create({ username, email, password, role });
+    const token = generateToken(user);
+
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      token
+    });
+  } catch (err) {
+    res.status(500).json({ msg: 'Server error' });
+  }
+};
+
+// @route   POST /api/login
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: 'Invalid credentials' });
+
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+
+    const token = generateToken(user);
+
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      token
+    });
+  } catch (err) {
+    res.status(500).json({ msg: 'Server error' });
+  }
+};
